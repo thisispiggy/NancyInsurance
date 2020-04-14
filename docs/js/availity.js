179d0e23c28076bfc90c04a6aa3fffd6937ac6bd
thisispiggy <thisispiggy@gmail.com>
2020-04-13 21:40:14 -0400

    added item

--- docs/js/availity.js
+++ docs/js/availity.js
@@ -99,12 +99,12 @@ fillDate = () => {
 
     // if first line and 99203 checked, filter out 99213, else filter out 99203
     if (line == 0 && filter99203[0].checked) {
-      let codes = cpt.filter(cpt => cpt.label !== 99213);
+      let codes = cpt.filter(item => item.label != 99213);
       // eslint-disable-next-line no-console
       console.log(99203 + codes);
       iterCpt(month, day, year, codes);
     } else {
-      let codes = cpt.filter(cpt => cpt.label !== 99203);
+      let codes = cpt.filter(item => item.label != 99203);
       // eslint-disable-next-line no-console
       console.log(99213 + codes);
       iterCpt(month, day, year, codes);
