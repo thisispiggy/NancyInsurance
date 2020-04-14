e9e0ffda52efa974679c049e6676d8dbcc941cd9
thisispiggy <thisispiggy@gmail.com>
2020-04-13 20:22:25 -0400

    added back const

--- docs/js/availity.js
+++ docs/js/availity.js
@@ -1,5 +1,5 @@
 // // eslint-disable-next-line no-undef
-// const { website, dates, diagnosisCode, cpt } = BOOKMARK;
+const { website, dates, diagnosisCode, cpt } = BOOKMARK;
 
 // eslint-disable-next-line no-undef
 const iframe = document.getElementById("newBodyFrame");
